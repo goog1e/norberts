025bfa845e62bd8ff28da85162b655112256da6f
Dave Justice <davejustishh@gmail.com>
2016-06-07 01:08:05 -0400

    send backups cron job, db, and error messaging

--- server.js
+++ server.js
@@ -7,6 +7,42 @@ var config = require('./config.json');
 var app = express();
 
 var db = require('./lib/db');
+var sendDump = require('./lib/send-dump.js');
+
+// initialize contacts cache
+var lastContactsCount = 0;
+var contactsCache = [];
+db.getContacts(function(contacts) {
+  contactsCache = contacts;
+  lastContactsCount = contacts.length;
+});
+
+// daily cronjob to send backups to admins
+var CronJob = require('cron').CronJob;
+new CronJob('00 30 11 * * 1-5',
+            weeklyTask,
+            cronJobComplete,
+            startCronNow,
+            timeZone);
+
+function cronJobComplete() {
+  console.log('CRONJON COMPLETE');
+}
+
+function weeklyTask() {
+  var dataDump = JSON.stringify(contactsCache, null, 2);
+  var contactCountDiff = contactsCache.length - lastContactsCount;
+  config.backupContacts.forEach(function(c) {
+    sendDump(c, contactCountDiff, dataDump);
+  });
+  fs.writeFile('stub-data/contacts.json', dataDump, function(err) {
+    if (err) console.error('BACKUP:ERROR:Error writing backup!!!! contact admin');
+    else {
+      console.log('BACKUP:SUCCESS:written successfully');
+    }
+  });
+  lastContactsCount = contactsCache.length;
+}
 
 app.use(basicAuth(config.auth.user, config.auth.password));
 
@@ -19,19 +55,27 @@ app.set('view engine', 'handlebars');
 app.use(logger('common'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
-  res.render('customers', {customers: fakeContacts});
-});
+app.get('/', homeView);
 
-app.post('/new', function(req,res) {
+function homeView(req, res, err) {
+  err = 'There was an error submitting data for Dave Justice try submitting that one again. If the problem continues contact davejustishh@gmail.com';
+  res.render('customers', {error: err,
+                           customers: contactsCache});
+}
+
+app.post('/new', function(req, res) {
   db.update(req.body, function(err) {
-    if (err) console.error('DB:ERROR writing ', req.body, ' to database. !!! ', err);
+    if (err) {
+      console.error('DB:ERROR writing ', req.body, ' to database. !!! ', err);
+      homeView(req, res, 'There was an error submitting data for ' + req.body.firstName + ' ' + req.body.lastName,
+               ' try submitting that one again. If the problem continues contact davejustishh@gmail.com');
+    }
     else {
       console.log('DB:SUCCESS writing', req.body, ' to database.');
+      contactsCache.push(req.body);
+      res.redirect('/');
     }
   });
-
-  res.redirect('/customers');
 });
 
 app.listen(config.port, function () {
